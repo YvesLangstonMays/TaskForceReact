eaf9e8bb4fb5a64615be7c79410252018783e3d4
TenomGlu <yveslangstonmays@gmail.com>
2023-05-30 18:30:57 -0500

    Fixed geolocations

--- src/trackers/components/MapTracking.tsx
+++ src/trackers/components/MapTracking.tsx
@@ -1,4 +1,5 @@
 import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 
 const API_Key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
@@ -7,45 +8,34 @@ const style = {
   width: "96%",
 };
 
-function updateLongitude(row: Locations, longitude: number) {
-  row.lng = longitude;
-}
-
-function updateLatitude(row: Locations, latitude: number) {
-  row.lat = latitude;
-}
-
-interface Locations {
-  lat: number;
-  lng: number;
-}
-
-let userLocation = {
-  lat: 1,
-  lng: 1,
-};
-
-navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
-  userLocation.lat = position.coords.latitude;
-  userLocation.lng = position.coords.longitude;
-});
-
 const center = {
   lat: 29.7377637,
   lng: -95.3328939,
 };
 
-console.log(userLocation);
-console.log(center);
-
 function MapTracking({ passedComp }: any) {
+  const [userLocation, setUserLocation] = useState({
+    lat: 1,
+    lng: 1,
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        setUserLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      }
+    );
+  }, []);
   const onLoad = () => {};
 
   return (
     <div className="theMap">
       <LoadScript googleMapsApiKey={API_Key}>
         <GoogleMap mapContainerStyle={style} zoom={9} center={center}>
-          <MarkerF onLoad={onLoad} position={center} />
+          <MarkerF onLoad={onLoad} position={userLocation} />
         </GoogleMap>
       </LoadScript>
     </div>
