c3cb718ef56cacd0940149990694e34fc9992531
TenomGlu <yveslangstonmays@gmail.com>
2023-05-29 21:03:39 -0500

    Deleting unused files

--- something.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-function componentDidMount() {
-  let data;
-  axios.get("http://localhost:8000").then((res) => {
-    data = res.data;
-    this.setState({ details: data });
-  });
-}
--- src/list-tracker/components/Location.tsx
+++
@@ -1,1 +0,0 @@
-export default { lat: 34.0522, lng: -118.2437 };
