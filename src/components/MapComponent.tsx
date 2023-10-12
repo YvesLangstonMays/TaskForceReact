e6777d8cdeadce8a45370bfde69429c5777ab138
TenomGlu <yveslangstonmays@gmail.com>
2023-10-12 11:39:30 -0500

    Changed Lat and Lng defaults for error checking

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,23 +1,24 @@
-import { RectangleF } from "@react-google-maps/api";
 import {
   GoogleMap,
   LoadScript,
   MarkerClustererF,
   MarkerF,
 } from "@react-google-maps/api";
+
 import { useEffect, useState } from "react";
 
 const API_Key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 const style = {
-  height: "25rem",
+  height: "28.5rem",
   width: "100%",
 };
 
 function MapComponent({ passedComp }: any) {
+  useEffect(() => {}, [GoogleMap]);
   const [userLocation, setUserLocation] = useState({
-    lat: 1,
-    lng: 1,
+    lat: 0,
+    lng: 0,
   });
 
   const locations = passedComp.map((val: { [x: string]: any }) => {
@@ -54,12 +55,10 @@ function MapComponent({ passedComp }: any) {
     return location.lat + location.lng;
   }
 
-  useEffect(() => {}, [GoogleMap]);
-
   return (
     <div className="theMap">
       <LoadScript googleMapsApiKey={API_Key}>
-        <GoogleMap mapContainerStyle={style} zoom={6} center={userLocation}>
+        <GoogleMap mapContainerStyle={style} zoom={10} center={userLocation}>
           <MarkerF position={userLocation} label={"You"} />
           <MarkerClustererF zoomOnClick={true}>
             {(clusterer) =>
